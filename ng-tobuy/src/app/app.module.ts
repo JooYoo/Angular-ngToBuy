76628d1f306da431cc94f15c0cbfd8c9f8cb105e
JooYoo <jooyoo@outlook.com>
2018-07-15 17:21:53 +0200

    installed material design

--- ng-tobuy/src/app/app.module.ts
+++ ng-tobuy/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+
 import { AppComponent } from './app.component';
+import { FormsModule } from '../../node_modules/@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
